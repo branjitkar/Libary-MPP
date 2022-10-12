ddd3df67c6232da70d861e72168df29e90360989
Bipul Ranjitkar <bipul.ran@gmail.com>
2022-10-12 14:56:56 -0500

    added function to UI for each mandatory usecase

--- LibraryProjectMpp/src/ui/SystemMenu.java
+++ LibraryProjectMpp/src/ui/SystemMenu.java
@@ -9,4 +9,20 @@ public class SystemMenu {
 	public SystemMenu(User user) {
 		this.user = user;
 	}
+
+	// TODO: UseCase1 - Utsab
+	public void addLibraryMember() {
+
+	}
+
+	// TODO: UseCase2 - Bipul
+	public void checkoutBook() {
+
+	}
+
+	// TODO: UseCase3 - Antonedei
+	public void addBook() {
+		// initially focus on adding a copy to an already existing book
+	}
+	
 }
